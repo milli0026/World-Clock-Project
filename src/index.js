904a116f73d69cd14c399bb80e83a0ea4e2eaadf
milli0026 <milli001@gmx.de>
2024-11-24 19:38:00 +0100

    js - automatic time update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 // Los Angeles
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
+function updateLosAngeles() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-)}`;
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+}
+
+setInterval(updateLosAngeles, 1000);
 
 // Sydney
-let sydneyElement = document.querySelector("#sydney");
-let sydneyDateElement = sydneyElement.querySelector(".date");
-let sydneyTimeElement = sydneyElement.querySelector(".time");
-let sydneyTime = moment().tz("Australia/Sydney");
+function updateSydney() {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
 
-sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-sydneyTimeElement.innerHTML = `${sydneyTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-)}`;
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = `${sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+}
+
+setInterval(updateSydney, 1000);
